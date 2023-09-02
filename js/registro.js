d720e5f8f129fdfa637e103caac5b671e0541316
PulpFiction1234 <rafaelalbertobenguria@gmail.com>
2023-09-02 09:30:47 -0400

    Menu hamburguesa desplegable

---
+++ js/registro.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    console.log("Función toggleMenu() ejecutada.");
+    var containerNav = document.querySelector('.container-nav');
+    containerNav.classList.toggle('menu-active');
+}
